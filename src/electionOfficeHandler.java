996b02873589b7a4601190c08bc2c1f82d3ff467
Wyatt Morris <morrisww@g.cofc.edu>
2016-11-18 13:21:37 -0500

    Commiting the Officer changes.

--- src/electionOfficeHandler.java
+++ src/electionOfficeHandler.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class electionOfficeHandler {
 	private electionOfficer officer;
@@ -7,21 +13,55 @@ public class electionOfficeHandler {
 		this.officer = officer;
 	}
 	
-	public void setOfficerID(int ID){
-		officer.setOfficerID(ID);
+	public void readOfficer(String userInput){
+		String fileLocation = File.separator + "Users" + File.separator + "wyattmorris" + File.separator + "git" + File.separator +  "e-voting" + File.separator +  "ElectionOfficer.txt";
+		//creates variable for the text file
+		File OfficerFile = new File(fileLocation);
+			
+			electionOfficer elec = new electionOfficer();
+			
+			try {
+				//creates the scanner that reads the file
+				Scanner officerScanner = new Scanner(OfficerFile);
+				while(officerScanner.hasNextLine()){			
+					//reads the first line of the file.
+					String nextLine = officerScanner.nextLine();
+			
+				
+					//Creates a list of strings for the current line of Officer ID info
+					String[] officerIDInfo = nextLine.split(",");
+					String currentID = officerIDInfo[0];
+					String currentName = officerIDInfo[1];
+					String currentPW = officerIDInfo[2];
+					
+					elec = new electionOfficer(currentID, currentName,currentPW);
+					//System.out.print("Officerr Name " + elec.getName() + " ");
+
+
+					if(currentID.equals(userInput)){
+						elec.setOfficerID(currentID);
+						elec.setName(currentName);
+						elec.setPass(currentPW);
+						break;
+					}
+					else{
+						elec = new electionOfficer();
+					}
+					
+				}
+				//end while loop
+				if(elec.getName() == null){
+					System.out.println("Officer ID not found.");
+				}
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
-	}
-	
-	public int getOfficerID(){
-		return officer.getID();
-	}
-	
-	public void setOfficerName(String name){
-		officer.setName(name);
-	}
-	
-	public String getName(){
-		return officer.getName();
+		
+		
+		
 	}
 	
 	
