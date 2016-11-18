79738f74c71fcf7a772b9a53ba6361e24ea09afc
Wyatt Morris <morrisww@g.cofc.edu>
2016-11-17 21:43:32 -0500

    committing something

---
+++ src/ReadOfficer.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadOfficer {
+
+	public static void main(String[] args) {
+		//creates a variable for path of the file
+		String fileLocation = File.separator + "Users" + File.separator + "wyattmorris" + File.separator + "git" + File.separator +  "e-voting" + File.separator +  "ElectionOfficer.txt";
+		//creates variable for the text file
+		File OfficerFile = new File(fileLocation);
+		
+		try {
+			//creates the scanner that reads the file
+			Scanner officerScanner = new Scanner(OfficerFile);
+			
+			//reads the first line of the file.
+			String nextLine = officerScanner.nextLine();
+			
+			System.out.println("Next line: " + nextLine);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		}
+	
+	
+}
