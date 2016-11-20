042b69decaa8b04d7214bfa12aead38bd46e63bc
Wyatt Morris <morrisww@g.cofc.edu>
2016-11-20 12:31:01 -0500

    committing old stuff

--- src/votingDriver.java
+++ src/votingDriver.java
@@ -56,14 +56,12 @@ public class votingDriver {
 	    	 electionOfficeHandler offHandler = new electionOfficeHandler();
 	    	 electionOfficer officer = offHandler.readOfficer(ID);
 	    	 
-//	    	 System.out.println("Hello, please enter Officer ID.");
-//	    	 String ID = sc.next();
-	    	 String realpass = officer.getPass().trim();
+
 	    	 System.out.println("(for testing purposes) Password should be: " + realpass);
 	    	 System.out.println("Please enter your password.");
 	    	 
 	    	 String pw = sc.next();
-	    	 System.out.println("this checks the pws: " + officer.getPass().trim().equals(realpass));
+
 	    	 if(pw.equals(officer.getPass().trim())){
 	    		 System.out.println("Please type the number for your next action.");
 	    		 System.out.println("To view results enter the value 0. To print results enter the value 1.");
